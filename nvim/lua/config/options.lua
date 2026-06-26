7891aa407b4f70f60b7a4be0346a143f26f34fb9
Atanas Ovcharov <a.ovcharov18@gmail.com>
2026-06-26 20:27:41 +0300

    feat(nvim): add cmake language server support
    - set neocmake as cmake language server

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,9 @@
--- Edit options
+-- Editor options
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
+vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-
-
+vim.opt.softtabstop = 4
+vim.opt.expandtab = false
+vim.g.c_syntax_for_h = 1
--- nvim/lua/lsp/servers.lua
+++ nvim/lua/lsp/servers.lua
@@ -2,36 +2,51 @@ local M = {}
 
 -- Each server function returns: { name = "...", config = {...} }
 function M.lua_ls(caps)
-  return {
-    name = "lua_ls",
-    config = {
-      capabilities = caps,
-      settings = { Lua = { diagnostics = { globals = { "vim" } } } },
-    },
-  }
+	return {
+		name = "lua_ls",
+		config = {
+			capabilities = caps,
+			settings = { Lua = { diagnostics = { globals = { "vim" } } } },
+		},
+	}
 end
 
 function M.clangd(caps)
-  return {
-    name = "clangd",
-    config = {
-      capabilities = caps,
-      cmd = {
-        "clangd",
-        "--background-index",
-        "--clang-tidy",
-        "--header-insertion=iwyu",
-        "--completion-style=detailed",
-        "--function-arg-placeholders",
-        "--fallback-style=llvm",
-      },
-      init_options = {
-        usePlaceholders = true,
-        completeUnimported = true,
-        clangdFileStatus = true,
-      },
-    },
-  }
+	return {
+		name = "clangd",
+		config = {
+			capabilities = caps,
+			cmd = {
+				"clangd",
+				"--background-index",
+				"--clang-tidy",
+				"--header-insertion=iwyu",
+				"--completion-style=detailed",
+				"--function-arg-placeholders",
+				"--fallback-style=llvm",
+			},
+			init_options = {
+				usePlaceholders = true,
+				completeUnimported = true,
+				clangdFileStatus = true,
+			},
+		},
+	}
+end
+
+function M.neocmake(caps)
+	return {
+		name = "neocmake",
+		config = {
+			capabilities = caps,
+			filetypes = { "cmake" },
+			init_options = {
+				format = { enable = false }, -- let gersemi handle formatting
+				lint = { enable = true },
+				scan_cmake_in_package = true,
+			},
+		},
+	}
 end
 
 return M
